8bfd899a48578541e2351684002ecdbfcd000b3b
RJC888 <121136782+RJC888@users.noreply.github.com>
2025-10-14 07:35:06 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'park-it-pin-it-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.js',
+  '/static/css/main.css',
+  'https://cdn.tailwindcss.com',
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
